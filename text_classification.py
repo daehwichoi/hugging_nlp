de3d1da11f8469861ad7208601717d99170037fc
daehwichoi <48538226+daehwichoi@users.noreply.github.com>
2023-11-23 22:24:57 +0900

    [Feature] 데이터 확인

--- text_classification.py
+++ text_classification.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datasets import list_datasets, load_dataset
+from collections import Counter
 
 if __name__ == '__main__':
     # all_dataset = list_datasets()
@@ -13,5 +14,21 @@ if __name__ == '__main__':
     # test_data = dataset["test"]
 
     train_df = pd.DataFrame(dataset['train'])
+    test_df = pd.DataFrame(dataset['test'])
     train_df['label_name'] = train_df["label"].apply(lambda x: dataset["train"].features["label"].int2str(x))
+    test_df['label_name'] = train_df["label"].apply(lambda x: dataset["test"].features["label"].int2str(x))
+
+    # Class 분포 확인
+    data = Counter(train_df['label_name'])
+    print("Train class 분포 : ", dict(data))
+    data = Counter(test_df['label_name'])
+    print("Test class 분포 : ", dict(data))
+    
+    # Token 개수 확인
+    train_df["num_of_token"] = train_df["text"].apply(lambda x : len(x.split(' ')))
+    test_df["num_of_token"] = test_df["text"].apply(lambda x : len(x.split(' ')))
     print(train_df)
+    print(test_df)
+
+    print("Train 최대 token", max(train_df["num_of_token"]))
+    print("Test 최대 token", max(test_df["num_of_token"]))
