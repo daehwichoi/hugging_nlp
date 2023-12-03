739125078c9775c3e521400a884d308ba99daa23
daehwichoi <48538226+daehwichoi@users.noreply.github.com>
2023-12-03 22:27:17 +0900

    [Feature]Student class loss 생성

--- nlp_model_tuning.py
+++ nlp_model_tuning.py
@@ -2,12 +2,44 @@ from datasets import load_dataset
 from datasets import load_metric
 
 from transformers import pipeline
+from transformers import TrainingArguments
+from transformers import Trainer
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
 from pathlib import Path
 import time
+
+
+class DistillationTrainer(Trainer):
+    def __init__(self, *args, teacher_model=None, **kwargs):
+        super().__init__()
+        self.teacher_model = teacher_model
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        outputs_stu = model(**inputs)
+        loss_ce = outputs_stu.loss
+        logits_stu = outputs_stu.logits
+
+        with torch.no_grad():
+            outputs_tea = self.teacher_model(**inputs)
+            logits_tea = outputs_tea.logits
+        loss_fct = nn.KLDivLoss(reduction="batchmean")
+        loss_hd = self.args.temperature ** 2 * loss_fct(F.log_softmax(logits_stu / self.args.temperature, dim=-1),
+                                                        F.softmax(logits_tea / self.args.temperature, dim=-1))
+
+        loss = self.args.alpha * loss_ce + (1. - self.args.alpha) * loss_hd
+        return (loss, outputs_stu) if return_outputs else loss
+
+
+class DistillationTrainingArguments(TrainingArguments):
+    def __init__(self, *args, alpha=0.5, temperature=2.0, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.alpha = alpha
+        self.temperature = temperature
 
 
 class PerformanceBenchmark:
