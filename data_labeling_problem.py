899053e892f3738d92501126a791fd2103219a89
daehwichoi <48538226+daehwichoi@users.noreply.github.com>
2023-12-11 22:15:28 +0900

    [Feature]훈련 데이터 전처리 변경

--- data_labeling_problem.py
+++ data_labeling_problem.py
@@ -1,14 +1,20 @@
+import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.model_selection import iterative_train_test_split
+from skmultilearn.model_selection import iterative_train_test_split
 
 
 def filter_labels(x):
     return [label_map[label] for label in x if label in label_map]
 
-def balanced_split(df, test_size = 0.5):
-    ind = 
+
+def balanced_split(df, test_size=0.5):
+    ind = np.expand_dims(np.arange(len(df)), axis=1)
+    print(df)
+    labels = mlb.transform(df["labels"])
+    ind_train, _, ind_test, _ = iterative_train_test_split(ind, labels, test_size)
+    return df.iloc[ind_train[:, 0]], df.iloc[ind_test[:, 0]]
 
 
 if __name__ == '__main__':
@@ -32,17 +38,10 @@ if __name__ == '__main__':
     df_issues["labels"] = df_issues["labels"].apply(filter_labels)
     all_labels = list(label_map.values())
 
-    print(value_count)
-
-    print(df_issues["labels"].head())
-
     df_issues["split"] = "unlabeled"
     mask = df_issues["labels"].apply(lambda x: len(x)) > 0
-    df_issues[mask]["split"] = "labeled"
+    df_issues.loc[mask,"split"] = "labeled"
     df_issues["split"].value_counts().to_frame()
-
-    print(df_issues)
-    print(df_issues.columns)
 
     for column in ["title", "body", "labels"]:
         print(f"{column} : {df_issues[column].iloc[26][:500]}\n")
@@ -56,3 +55,13 @@ if __name__ == '__main__':
     mlb = MultiLabelBinarizer()
     mlb.fit([all_labels])
     print(mlb.transform([["tokenization", "new model"], ["pytorch"]]))
+
+    df_clean = df_issues[["text", "labels", "split"]].reset_index(drop=True).copy()
+    df_unsup = df_clean.loc[df_clean["split"] == 'unlabeled', ["text", "labels"]]
+    df_sup = df_clean.loc[df_clean["split"] == 'labeled', ["text", "labels"]]
+    print(df_sup)
+
+    np.random.seed(0)
+    df_train, df_tmp = balanced_split(df_sup, test_size=0.5)
+    df_valid, df_test = balanced_split(df_tmp, test_size=0.5)
+    print(df_train)
